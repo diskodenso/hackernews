d223ec0c3b1396bdd7adf03097923fc27fa69f07
Denso666 <Denisolf@mx.de>
2022-04-21 18:26:51 +0200

    make query Search responsive

--- src/App.js
+++ src/App.js
@@ -2,17 +2,39 @@ import './App.css';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import SearchBar from './components/SearchBar';
-import Home from './views/Home';
-
 
 function App() {
+  const [articles, setArticles] = useState();
+  // create a state for search input
+  const [query, setQuery] = useState('vue3');
+  // fetch the data with useEffect and safe in state
+  
 
+ useEffect(() => {
+   axios
+      // get the data from the specific endpoint - in this case search by text "vue3"
+     .get("https://hn.algolia.com/api/v1/search_by_date?query=${query}&page=10&tags=story")
+           // transcript the data you fetching from json into javascript for readability
+      .then((response) => setArticles(response.data.hits))
+      .catch((err) => console.log(err));
+ }, []);
+  
   return (
     <div className="App">
       <SearchBar />
-      <Home/>
+    
+			{articles
+				? articles.map((article) => (
+          <div key={article.objectID}>
+						<p>{article.title}</p>
+            <p>{article.author}</p>
+            <p>{article.num_comments}</p>
+            <p>{article.created_at}</p>
+            </div>
+				  ))
+				: 'Loading...'}
       </div>
-  );
+	);
 }
 
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,13 @@
 import React from 'react'
+import './Style.css';
 
 function SearchBar(placeholder, data) {
+
   return (
       <div className='search'>
           <div className='searchInputs'>
               <input type="text" placeholder='Search' />
-              <div className='searchIcon'>                 
-</div>
-          </div>
+            </div>
       </div>
   )
 }
