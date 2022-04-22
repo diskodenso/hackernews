608e87e788d0341e679762a593e56fb0c9c687d2
Denso666 <Denisolf@mx.de>
2022-04-22 12:41:17 +0200

    add form to SearchBar Component

--- src/App.js
+++ src/App.js
@@ -5,35 +5,41 @@ import SearchBar from './components/SearchBar';
 
 function App() {
   const [articles, setArticles] = useState();
-  // create a state for search input
-  const [query, setQuery] = useState('vue3');
-  // fetch the data with useEffect and safe in state
+  console.log(articles);
+  const [queries, setQueries] = useState('');
+
+  const handleChange = (event) => {
+	  console.log(event.target.value);
+	  return 
+  }
   
 
- useEffect(() => {
-   axios
-      // get the data from the specific endpoint - in this case search by text "vue3"
-     .get("https://hn.algolia.com/api/v1/search_by_date?query=${query}&page=10&tags=story")
-           // transcript the data you fetching from json into javascript for readability
-      .then((response) => setArticles(response.data.hits))
-      .catch((err) => console.log(err));
- }, []);
+  // fetch the data with useEffect and safe in state
+	useEffect(() => {
+		axios
+			// get the data from the specific endpoint - in this case search by text "vue3"
+			.get(
+				`https://hn.algolia.com/api/v1/search_by_date?query=${queries}&page=10&tags=story`
+			)
+			// transcript the data you fetching from json into javascript for readability
+			.then((response) => setArticles(response.data.hints))
+			.catch((err) => console.log(err));
+  }, []);
   
+	// return jsx!!! with tenary operators
   return (
     <div className="App">
-      <SearchBar />
+      <SearchBar handleChange={handleChange}/>
     
 			{articles
 				? articles.map((article) => (
-          <div key={article.objectID}>
-						<p>{article.title}</p>
-            <p>{article.author}</p>
-            <p>{article.num_comments}</p>
-            <p>{article.created_at}</p>
-            </div>
+						<div key={article.objectID}>
+							<h2>{article.title}</h2>
+							<p>{article.author}</p>
+						</div>
 				  ))
-				: 'Loading...'}
-      </div>
+				: 'Loading.....'}
+		</div>
 	);
 }
 
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,11 +4,13 @@ import './Style.css';
 function SearchBar(placeholder, data) {
 
   return (
+    <form>
       <div className='search'>
           <div className='searchInputs'>
               <input type="text" placeholder='Search' />
             </div>
       </div>
+    </form>
   )
 }
 
